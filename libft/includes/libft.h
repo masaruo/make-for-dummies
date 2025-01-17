fbc7bb484ba206b30ccf9e642a3dcffae0bc7a92
masaru ogawa <masaruo@gmail.com>
2025-01-17 16:06:38 +0900

    libft level completed

---
+++ libft/includes/libft.h
@@ -0,0 +1,4 @@
+#pragma once
+
+void func(void);
+void func1(void);
---
+++ libft/src/main.c
@@ -0,0 +1,10 @@
+#include "libft.h"
+#include <stdio.h>
+
+int main(void)
+{
+	func();
+	func1();
+	printf("this is libft\n");
+	return (0);
+}
